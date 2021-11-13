dd03d1ab2e3f37a0dc38055e78e4e871e01cdc4a
Alessandro Dieni <alessandro.dieni>
2021-11-13 19:56:51 +0100

    Init Project

---
+++ src/components/RopeDataSheet.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-container>
+    <v-card class="pa-2">
+      <v-card-title>Rope Data Sheet</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="purchaser"
+              label="Committente"
+              dense
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="orderNumber"
+              label="Numero Ordine"
+              dense
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="intenderFor"
+              label="Destinazione"
+              dense
+              outlined
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-select
+              :items="diameters"
+              label="Diametro (mm)"
+              dense
+              outlined
+              @change="onDiameterChange()"
+            >
+            </v-select>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="length"
+              label="Lunghezza"
+              dense
+              outlined
+              readonly
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="linearMass"
+              label="Massa Lineare"
+              dense
+              outlined
+              readonly
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="yarnsNumberAndType"
+              label="Numero e tipo dei fili"
+              dense
+              outlined
+              readonly
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="ropeMass"
+              label="Massa del cavo"
+              dense
+              outlined
+              readonly
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="minimumBreakingStrength"
+              label="Carico di rottura minimo"
+              dense
+              outlined
+              readonly
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="generateAndDownloadPDF()" color="primary"
+          >Genera & Scarica PDF</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class RopeDataSheet extends Vue {
+  private purchaser = "";
+  private orderNumber = "";
+  private intenderFor = "";
+  private diameters: number[] = [22, 23, 24];
+  private length = 0;
+  private linearMass = 0;
+  private yarnsNumberAndType = "";
+  private ropeMass = 0;
+  private minimumBreakingStrength = "";
+
+  onDiameterChange(): void {
+    this.length = Math.round(Math.random() * 100);
+    this.linearMass = Math.round(Math.random() * 100);
+    this.yarnsNumberAndType = Math.round(Math.random() * 100).toString();
+    this.ropeMass = Math.round(Math.random() * 100);
+    this.minimumBreakingStrength = Math.round(Math.random() * 100).toString();
+  }
+  generateAndDownloadPDF(): void {
+    alert("Not Implemented yet!");
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <rope-data-sheet></rope-data-sheet>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import RopeDataSheet from "@/components/RopeDataSheet.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    RopeDataSheet,
   },
 })
 export default class Home extends Vue {}
